2ad34fbc9d19474940b0cf85c87950a800aab58f
Craig Hopgood <searaig@gmail.com>
2026-05-13 13:53:26 -0400

    add sketch types

---
+++ src/sketch/types.ts
@@ -0,0 +1,46 @@
+import type P5 from "p5";
+import { AudioInputApi } from "../audio/audio-input";
+import { DataBroker } from "../streams/broker";
+import { DataProducer } from "../streams/producer";
+
+export type SketchMeta = {
+    id: string;
+    name: string;
+    status: 'publish' | 'draft';
+    permissions: {
+        microphone?: boolean;
+    }
+    tags: string[];
+}
+
+// Sketch Context
+export type SketchRole = 'generator' | 'consumer'
+
+export type SketchEventApi = {
+    loaded: ( p: P5 ) => void;
+    ended: ( p: P5 ) => void;
+}
+
+type BaseSketchContext = {
+    p5: typeof P5;
+    canvas: HTMLElement;
+    notify: SketchEventApi;
+    audio?: AudioInputApi;
+}
+
+export type ConsumerContext = BaseSketchContext & {
+    broker?: DataBroker | undefined;
+    producer?: never;
+}
+
+export type GeneratorContext = BaseSketchContext & {
+    broker?: never;
+    producer?: DataProducer | undefined;
+}
+
+export type SketchContext = ConsumerContext | GeneratorContext
+
+export type SketchModule = {
+    meta: Record<string, unknown>;
+    create: ( context: SketchContext ) => Promise<P5>;
+}
