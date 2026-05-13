77052c850b40cfd984f357060efce0084034ab1a
Craig Hopgood <searaig@gmail.com>
2026-05-13 13:35:44 -0400

    add DataBroker

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,1 @@
-
+export * from "./streams/broker"
---
+++ src/streams/broker.ts
@@ -0,0 +1,67 @@
+export interface DataBroker {
+    /* receives data ... ingress */
+    push( msg: Uint8Array | Record<string, unknown> | string, senderId?: number ): void;
+    canPush( data: unknown ): data is Uint8Array | string | Record<string, unknown>;
+    onData( fn: ( bytes: Uint8Array, senderId: number ) => void ): () => void;
+    sendData( bytes: Uint8Array, senderId?: number ): void;
+    onMeta( fn: ( data: Record<string, unknown>, senderId: number ) => void ): () => void;
+    sendMeta( data: Record<string, unknown>, senderId?: number ): void;
+}
+
+export function createDataBroker(): DataBroker {
+    let onDataCallback: (( bytes: Uint8Array, senderId: number ) => void) | null = null
+    let onMetaCallback: (( data: Record<string, unknown>, senderId: number ) => void) | null = null
+
+    return {
+
+        canPush( data ): data is Uint8Array | string | Record<string, unknown> {
+            return data instanceof Uint8Array || typeof data === 'string' || (typeof data === 'object' && data !== null)
+        },
+
+        push( data, senderId = 0 ) {
+            if ( data instanceof Uint8Array || data instanceof ArrayBuffer ) {
+                // only send uint8array
+                onDataCallback?.(
+                    data instanceof ArrayBuffer ? new Uint8Array( data ) : data,
+                    senderId
+                )
+            } else if ( typeof data === 'string' ) {
+                // send strings as meta
+                try {
+                    onMetaCallback?.( JSON.parse( data ), senderId )
+                } catch ( e ) {
+                    console.warn( `Datastream got non-JSON string ... skipping data='${ data }'` )
+                }
+            } else {
+                // meta send expects json
+                onMetaCallback?.( data, senderId )
+            }
+        },
+
+        sendData( bytes, senderId = 0 ) {
+            onDataCallback?.( bytes, senderId )
+        },
+
+        sendMeta( data, senderId = 0 ) {
+            onMetaCallback?.( data, senderId )
+        },
+
+        onData( fn ) {
+            onDataCallback = fn
+
+            return () => {
+                if ( onDataCallback === fn ) onDataCallback = null;
+            }
+        },
+
+        onMeta( fn ) {
+            onMetaCallback = fn
+
+            return () => {
+                if ( onMetaCallback === fn ) onMetaCallback = null;
+            }
+        },
+    }
+}
+
+
