6bd27b277de517703a9bade0277603fd032bd421
Craig Hopgood <searaig@gmail.com>
2026-05-27 12:18:36 -0400

    delete + rename Sketch2 -> Sketch

--- src/sketch/meta.ts
+++ src/sketch/meta.ts
@@ -1,3 +1,4 @@
+
 export type SketchMeta = {
     id: string;
     name: string;
@@ -7,3 +8,44 @@ export type SketchMeta = {
     }
     tags: string[];
 }
+
+/** Params */
+export type SketchParam =
+    | RangeParam
+    | SelectParam
+
+export type SketchParams = {
+    [key: string]: SketchParam
+}
+
+export type ParamChangeHandler = {
+    ( param: RangeParam, newValue: number ): void;
+    ( param: SelectParam, newValue: string ): void;
+}
+
+export type ParamOption = {
+    value: string
+    label: string
+}
+
+/** RangeParam */
+export type RangeParam = {
+    type: 'range'
+    id: string
+    label: string
+    value: number
+    min: number
+    max: number
+    step: number
+}
+
+
+/** SelectParam */
+export type SelectParam = {
+    type: 'select'
+    id: string
+    label: string
+    value?: string
+    options: ParamOption[] | (() => Promise<ParamOption[]>)
+}
+
