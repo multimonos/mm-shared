7f857d84685c0b1872d9eaa2e394114648e6a12a
Craig Hopgood <searaig@gmail.com>
2026-05-27 14:39:03 -0400

    more type fixing

--- src/sketch/context.ts
+++ src/sketch/context.ts
@@ -3,32 +3,45 @@ import { type P5, type P5Constructor } from "../p5-extended"
 import { DataBroker } from "../streams/broker";
 import { DataProducer } from "../streams/producer";
 
+/** Core */
+type BaseSketchContext = {
+    p5: P5Constructor
+    canvas: HTMLElement
+    notify: SketchNotifier
+    audio?: AudioInputApi
+}
+
+/** As a factory we are just injecting dependencies from the framework */
+export type SketchFactoryContext = BaseSketchContext & {
+    broker?: DataBroker
+    producer?: DataProducer
+}
+
+
+/** As a sketch we can assert which values we consume from the framework */
 export type SketchContext =
     | ConsumerContext
     | GeneratorContext
+    | TransceiverContext
 
-type BaseSketchContext = {
-    p5: P5Constructor;
-    canvas: HTMLElement;
-    notify: SketchNotifier;
-    audio?: AudioInputApi;
-}
-
+/** Sketch contexts */
 export type ConsumerContext = BaseSketchContext & {
-    broker?: DataBroker | undefined;
-    producer?: never;
+    broker: DataBroker; // required
+    producer?: DataProducer; // optional
 }
 
 export type GeneratorContext = BaseSketchContext & {
-    broker?: never;
-    producer?: DataProducer | undefined;
+    broker?: DataBroker; // optional
+    producer: DataProducer; // required
 }
 
 export type TransceiverContext = BaseSketchContext & {
-    broker?: never;
-    producer?: DataProducer | undefined;
+    broker: DataBroker;
+    producer: DataProducer;
 }
 
+
+/** Dependencies */
 export type SketchNotifier = {
     loaded: ( p: P5 ) => void;
     ended: ( p: P5 ) => void;
--- src/sketch/module.ts
+++ src/sketch/module.ts
@@ -1,8 +1,10 @@
 import type { P5 } from "../p5-extended"
-import { SketchContext } from "./context";
+import { type SketchFactoryContext } from "./context";
 import type { SketchMeta } from "./meta";
 
 export interface SketchModule {
     meta: SketchMeta,
-    create: ( context: SketchContext ) => Promise<P5>;
+    create: SketchFactory
 }
+
+export type SketchFactory = ( context: SketchFactoryContext ) => Promise<P5>
