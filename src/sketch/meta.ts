47aa368aea27ef875b38f49ed9e69ef6c1eab1ff
Craig Hopgood <searaig@gmail.com>
2026-06-05 15:03:10 -0400

    typefix

--- src/sketch/meta.ts
+++ src/sketch/meta.ts
@@ -27,7 +27,7 @@ export type SketchVariant = {
     name: string
     audioUrl: string
     isDefault: boolean
-    params?: Record<string,any> // use as passthru params for later access
+    params?: Record<string, any> // use as passthru params for later access
 }
 
 /** Params */
