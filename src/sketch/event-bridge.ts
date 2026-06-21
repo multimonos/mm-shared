d7f4e03525b5cd82bac9d5eb91f9f40e05ef10f4
Craig Hopgood <searaig@gmail.com>
2026-06-21 15:43:10 -0700

    + unsub and destroy for event bridge

--- src/sketch/event-bridge.ts
+++ src/sketch/event-bridge.ts
@@ -8,22 +8,33 @@ export type SketchEventBridge = ReturnType<typeof createEventBridge>
 
 export function createEventBridge() {
 
-    const listeners: { [K in keyof SketchEvents]?: SketchEvents[K][] } = {}
+    let listeners: { [K in keyof SketchEvents]?: SketchEvents[K][] } = {}
 
     function on<K extends keyof SketchEvents>( event: K, fn: SketchEvents[K] ) {
         if ( ! listeners[event] ) {
             listeners[event] = []
         }
         listeners[event]!.push( fn )
+
+        // unsub
+        return () => {
+            if ( ! listeners[event] ) return;
+            listeners[event] = listeners[event]!.filter( listener => listener !== fn )
+        }
     }
 
     function emit<K extends keyof SketchEvents>( event: K, ...args: Parameters<SketchEvents[K]> ) {
         listeners[event]?.forEach( fn => (fn as any)( ...args ) )
     }
 
+    function destroy() {
+        listeners = {}
+    }
+
     return {
         on,
         emit,
+        destroy,
     }
 
 }
