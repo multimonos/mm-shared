7b2d2761c4fb005941da02e6fccc7205d862d8b0
Craig Hopgood <searaig@gmail.com>
2026-07-02 16:41:11 -0400

    only send data to open connections

--- src/streams/transport/peer-producer.ts
+++ src/streams/transport/peer-producer.ts
@@ -1,5 +1,5 @@
-import type { DataProducer } from "../producer";
 import type { DataConnection } from "peerjs";
+import type { DataProducer } from "../producer";
 
 export function createPeerDataProducer( conn: DataConnection ): DataProducer {
     return {
@@ -7,16 +7,19 @@ export function createPeerDataProducer( conn: DataConnection ): DataProducer {
         type: 'peer-producer',
 
         push( data ) {
+            if ( ! conn.open ) return; // Never push to closed.
             conn.send( data );
             // console.log( 'producer.push', data )
         },
 
         sendData( bytes: Uint8Array ) {
+            if ( ! conn.open ) return; // Never push to closed.
             conn.send( bytes );
             // console.log( 'producer.sendData', bytes )
         },
 
-        sendMeta( meta:Record<string, unknown> ) {
+        sendMeta( meta: Record<string, unknown> ) {
+            if ( ! conn.open ) return; // Never push to closed.
             conn.send( meta );
             // console.log( 'producer.sendMeta',meta )
         },
